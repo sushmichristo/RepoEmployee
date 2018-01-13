f60d093590901054b1349a3602f551be10b69b1a
Sushmi <sushmichristo@gmail.com>
2018-01-13 14:37:09 +0530

    file commit

--- Employee/src/main/java/com/workplace/employee/controller/EmployeeController.java
+++ Employee/src/main/java/com/workplace/employee/controller/EmployeeController.java
@@ -14,37 +14,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.workplace.employee.model.Employee;
 import com.workplace.employee.service.EmployeeService;
+import com.workplace.employee.util.Validate;
 
 @RestController
 @CrossOrigin
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
-	
+
 	@GetMapping("/getAllEmployees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return this.employeeService.getAllEmployees();
 	}
-	
+
 	@GetMapping("/getEmployee/{id}")
-	public Employee getEmployee(@PathVariable int id){
+	public Employee getEmployee(@PathVariable int id) {
 		return this.employeeService.getEmployee(id);
 	}
-	
+
 	@PostMapping("/addEmployee")
-	public boolean addEmployee(@RequestBody Employee employee){
-		return this.employeeService.addEmployee(employee);
+	public String addEmployee(@RequestBody Employee employee) {
+		Validate.notNull(employee);
+		final List<Employee> existingEmployees = this.employeeService.getAllEmployees();
+		if (!existingEmployees.contains(employee)) {
+			this.employeeService.saveEmployee(employee);
+			return "Emplyee has successfully added";
+		}
+		return "Employee already exists";
 	}
-	
+
 	@PutMapping("/editEmployee/{id}")
-	public void editEmployee(@PathVariable int id, @RequestBody Employee employee){
-		this.employeeService.updateEmployee(id,employee);
+	public String editEmployee(@PathVariable int id, @RequestBody Employee employee) {
+		Validate.notNull(employee);
+		this.employeeService.saveEmployee(employee);
+		return "Employee has successfully updated";
 	}
-	
+
 	@DeleteMapping("/deleteEmployee/{id}")
-	public void deleteEmployee(@PathVariable int id){
+	public void deleteEmployee(@PathVariable int id) {
 		this.employeeService.deleteEmployee(id);
 	}
 }
